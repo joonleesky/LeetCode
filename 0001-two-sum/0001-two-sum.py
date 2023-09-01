e4089e0b583ef96c1faa753378423fa51f4d6800
joonleesky <joonleesky90@gmail.com>
2023-09-01 17:10:16 +0900

    Time: 31 ms (97.65%), Space: 14.3 MB (32.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,6 +6,8 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        # brute-force solution
+        """
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 num1 = nums[i]
@@ -13,3 +15,15 @@ class Solution(object):
                 
                 if num1 + num2 == target:
                     return [i, j]
+        """
+        # hash-map solution
+        hash_map = {}
+        for idx, num in enumerate(nums):
+            hash_map[num] = idx
+            
+        for idx, num in enumerate(nums):
+            _target = target - num
+            if _target in hash_map:
+                trg_idx = hash_map[_target]
+                if idx != trg_idx:
+                    return [idx, hash_map[_target]]
